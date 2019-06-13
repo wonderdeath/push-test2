17c75c654d12c223f4c0024cf8dfa293a786c2d4
wonderdeath <39296168+wonderdeath@users.noreply.github.com>
2019-06-13 18:35:21 +0600

    Create trls.js

---
+++ js/trls.js
@@ -0,0 +1,214 @@
+var translation = {
+	source: {
+		title: 'Click Allow',
+		text1: 'Click В«AllowВ» to confirm that you are not a robot!'
+	},
+	en: {
+		title: 'Click Allow',
+		text1: 'Click В«AllowВ» to confirm that you are not a robot!'
+	},
+	ar: {
+		title: 'Ш§Щ†Щ‚Ш± ШЄЩ…ЩѓЩЉЩ†',
+		text1: 'Ш§Щ†Щ‚Ш± ЩЃЩ€Щ‚ "Ш§Щ„ШіЩ…Ш§Ш­" Щ„ШЄШЈЩѓЩЉШЇ ШЈЩ†Щ‘Щѓ Щ„ШіШЄ ШҐЩ†ШіШ§Щ†Щ‹Ш§ ШўЩ„ЩЉЩ‹Ш§!',
+	},
+	bg: {
+		title: 'РќР°С‚РёСЃРЅРµС‚Рµ Р Р°Р·СЂРµС€Рё',
+		text1: 'РљР»РёРєРЅРё В«Р Р°Р·СЂРµС€РёВ», Р·Р° РґР° РїРѕС‚РІСЉСЂРґРёС€, С‡Рµ РЅРµ СЃРё СЂРѕР±РѕС‚!',
+	},	
+	cs: {
+		title: 'StisknД›te Povolit',
+		text1: 'KliknutГ­m na В«PovolitВ» potvrДЏte, Еѕe nejste robot!',
+	},
+	nl: {
+		title: 'Druk op Toestaan',
+		text1: 'Klik op \'Toestaan\' om te bevestigen dat je geen robot bent!',
+	},	
+	fr: {
+		title: 'Appuyez sur Autoriser',
+		text1: 'Cliquez sur В« autoriser В» pour confirmer que vous n\'ГЄtes pas un robot !',
+	},
+	de: {
+		title: 'Zulassen drГјcken',
+		text1: 'Klicken Sie auf вЂћErlaubenвЂњ, um zu bestГ¤tigen, dass Sie kein Roboter sind!',
+	},	
+	hu: {
+		title: 'Nyomj az EngedГ©lyezГ©sre',
+		text1: 'Kattints az В«EngedГ©lyezВ» gombra annak megerЕ‘sГ­tГ©sГ©hez, hogy nem vagy robot!',
+	},
+	it: {
+		title: 'Premi Autorizza',
+		text1: 'Clicca su В«ConsentiВ» per confermare di non essere un robot!',
+	},
+	ja: {
+		title: 'гЂЊиЁ±еЏЇгЂЌг‚’жЉјгЃ—гЃѕгЃ™',
+		text1: 'В«иЁ±еЏЇВ»г‚’г‚ЇгѓЄгѓѓг‚ЇгЃ—гЃ¦гЂЃгѓ­гѓњгѓѓгѓ€гЃ§гЃЇгЃЄгЃ„гЃ“гЃЁг‚’зўєиЄЌгЃ—гЃ¦гЃЏгЃ гЃ•гЃ„пјЃ',
+	},
+	ko: {
+		title: 'н—€мљ©мќ„ л€„лҐґм„ёмљ”',
+		text1: 'лЎњлґ‡мќґ м•„л‹€лќјлЉ” кІѓмќ„ н™•мќён•л ¤л©ґ В«AllowВ»лҐј нЃґл¦­н•м„ёмљ”!',
+	},	
+	pl: {
+		title: 'NaciЕ›nij przycisk ZezwГіl',
+		text1: 'Kliknij вЂћZezwГіlвЂќ, by potwierdziД‡, Ејe nie jesteЕ› robotem!',
+	},
+	ro: {
+		title: 'ApДѓsaИ›i вЂћPermiteИ›iвЂќ',
+		text1: 'DaИ›i clic pe вЂћPermiteвЂќ pentru a confirma cДѓ nu sunteИ›i robot!',
+	},	
+	ru: {
+		title: 'РќР°Р¶РјРёС‚Рµ "Р Р°Р·СЂРµС€РёС‚СЊ"',
+		text1: 'РќР°Р¶РјРёС‚Рµ В«Р Р°Р·СЂРµС€РёС‚СЊВ» РґР»СЏ РїРѕРґС‚РІРµСЂР¶РґРµРЅРёСЏ С‡С‚Рѕ Р’С‹ РЅРµ СЂРѕР±РѕС‚!',
+	},
+	sr: {
+		title: 'РџСЂРёС‚РёСЃРЅРёС‚Рµ Р”РѕР·РІРѕР»Рё',
+		text1: 'РљР»РёРєРЅРёС‚Рµ РЅР° В«Р”РѕР·РІРѕР»РёВ» РґР° Р±РёСЃС‚Рµ РїРѕС‚РІСЂРґРёР»Рё РґР° РЅРёСЃС‚Рµ СЂРѕР±РѕС‚!',
+	},	
+	es: {
+		title: 'Pulsa en Permitir',
+		text1: 'ВЎHaz clic en "Permitir" para confirmar que no eres un robot!',
+	},
+	th: {
+		title: 'аёЃаё”аё­аё™аёёаёЌаёІаё•',
+		text1: 'аё‰аё„аёҐаёґаёЃ В«аё­аё™аёёаёЌаёІаё•В» а№Ђаёћаё·а№€аё­аёўаё·аё™аёўаё±аё™аё§а№€аёІаё„аёёаё“а№„аёЎа№€а№ѓаёЉа№€аё«аёёа№€аё™аёўаё™аё•а№Њ!',
+	},
+	uk: {
+		title: 'РќР°С‚РёСЃРЅС–С‚СЊ Р”РѕР·РІРѕР»РёС‚Рё',
+		text1: 'РќР°С‚РёСЃРЅС–С‚СЊ "Р”РѕР·РІРѕР»РёС‚Рё", С‰РѕР± РґРѕРІРµСЃС‚Рё, С‰Рѕ РІРё РЅРµ СЂРѕР±РѕС‚!',
+
+	},
+	zh: {
+		title: 'жЊ‰дё‹е…Ѓи®ё',
+		text1: 'з‚№е‡»В«е…Ѓи®ёВ»пјЊд»ҐзЎ®и®¤ж‚Ёе№¶йќћжњєе™ЁдєєпјЃ',
+	},	
+	fa: {
+		title: 'Ш§Ш¬Ш§ШІЩ‡ ШЇШ§ШЇЩ† Ш±Ш§ ШЁШІЩ†ЫЊШЇ ',
+		text1: 'Ш±Щ€ЫЊ В«Ш§Ш¬Ш§ШІЩ‡ ШЇШ§ШЇЩ†В» ШЁШІЩ†ЫЊШЇ ШЄШ§ Щ…ШґШ®Шµ ШґЩ€ШЇ Ъ©Щ‡ Ш±ШЁШ§ШЄ Щ†ЫЊШіШЄЫЊШЇ!',
+	},
+	pt: {
+		title: 'Prima Permitir',
+		text1: 'Clique em В«PermitirВ» para confirmar que nГЈo Г© um robГґ!',
+	},
+	no: {
+		title: 'Klikk Tillat',
+		text1: 'Klikk В«TillatВ» for ГҐ bekrefte at du ikke er en robot!',
+	},
+	sk: {
+		title: 'Kliknite na PovoliЕҐ',
+		text1: 'KliknutГ­m na tlaДЌidlo "PovoliЕҐ" potvrДЏte, Еѕe nie ste robot!',
+	},	
+	el: {
+		title: 'ОљО¬ОЅП„Оµ ОєО»О№Оє ПѓП„О·ОЅ ОµПЂО№О»ОїОіО® ОќО± ОµПЂО№П„ПЃО­ПЂОµП„О±О№',
+		text1: 'ОљО¬ОЅП„Оµ ОєО»О№Оє ПѓП„Ої ОєОїП…ОјПЂОЇ В«О•ПЂО№П„ПЃО­ПЂОµП„О±О№В» ОіО№О± ОЅО± ОµПЂО№ОІОµОІО±О№ПЋПѓОµП„Оµ ПЊП„О№ ОґОµОЅ ОµОЇПѓП„Оµ ПЃОїОјПЂПЊП„!',
+	},
+	lv: {
+		title: 'SpustelД—kite Leisti',
+		text1: 'SpustelД—kite В«LeistiВ», kad patvirtintumД—te, jog nesate robotas!',
+	},
+	lt: {
+		title: 'NoklikЕЎД·iniet uz AtДјaut',
+		text1: 'NoklikЕЎД·iniet uz В«AtДјautВ», lai apstiprinДЃtu, ka neesat robots!',
+	},
+	ee: {
+		title: 'KlГµpsake nuppu Luba',
+		text1: 'Vajuta В«LubaВ», et kinnitada, et te ei ole robot!',
+	},
+	hi: {
+		title: 'а¤…а¤ЁаҐЃа¤®а¤¤а¤ї а¤¦аҐ‡а¤‚ а¤Єа¤° а¤•аҐЌа¤Іа¤їа¤• а¤•а¤°аҐ‡а¤‚',
+		text1: 'В«а¤ЄаҐЃа¤·аҐЌа¤џа¤ї а¤•а¤°аҐ‡а¤‚В» а¤Єа¤° а¤•аҐЌа¤Іа¤їа¤• а¤•а¤°аҐ‡а¤‚ а¤•а¤ї а¤†а¤Є а¤°аҐ‹а¤¬аҐ‹а¤џ а¤Ёа¤№аҐЂа¤‚ а¤№аҐ€а¤‚!',
+	},	
+	my: {
+		title: 'Klik Benarkan',
+		text1: 'Klik В«BenarkanВ» untuk mengesahkan bahawa anda bukan robot!',
+	},
+	hr: {
+		title: 'Kliknite Dopusti',
+		text1: 'Kliknite В«DopustiВ» da biste potvrdili da niste robot!',
+	},
+	bn: {
+		title: 'а¦…а¦Ёа§Ѓа¦®а¦¤а¦ї а¦¦а¦їа¦Ё а¦•а§Ќа¦Іа¦їа¦• а¦•а¦°а§Ѓа¦Ё',
+		text1: 'а¦†а¦Єа¦Ёа¦ї а¦Џа¦•а¦џа¦ї а¦°а§‹а¦¬а¦џ а¦Ёа¦ѕ а¦Ёа¦їа¦¶а§Ќа¦ља¦їа¦¤ а¦•а¦°а¦¤а§‡ В«а¦…а¦Ёа§Ѓа¦®а¦¤а¦їВ» а¦•а§Ќа¦Іа¦їа¦• а¦•а¦°а§Ѓа¦Ё!'
+	},	
+	tr: {
+		title: 'Д°zin Ver DГјДџmesine Bas',
+		text1: 'Robot olmadД±ДџД±nД± onaylamak iГ§in В«Д°zin VerВ» dГјДџmesine tД±kla!',
+	}
+};
+
+var rtlLangs = ['ar', 'fa'];
+
+var browserLang = detect_language();
+var siteLang = getParameterByName("siteLang", false);
+var extTpl = getParameterByName("extTpl", 1);
+
+/** docReady provides a method of scheduling one or more javascript functions to run at some later point when the DOM has finished loading. */
+!function(t,e){"use strict";function n(){if(!a){a=!0;for(var t=0;t<o.length;t++)o[t].fn.call(window,o[t].ctx);o=[]}}function d(){"complete"===document.readyState&&n()}t=t||"docReady",e=e||window;var o=[],a=!1,c=!1;e[t]=function(t,e){return a?void setTimeout(function(){t(e)},1):(o.push({fn:t,ctx:e}),void("complete"===document.readyState||!document.attachEvent&&"interactive"===document.readyState?setTimeout(n,1):c||(document.addEventListener?(document.addEventListener("DOMContentLoaded",n,!1),window.addEventListener("load",n,!1)):(document.attachEvent("onreadystatechange",d),window.attachEvent("onload",n)),c=!0)))}}("docReady",window);
+
+function detect_language() {
+	var userLang = navigator.languages && navigator.languages[0] || navigator.language || navigator.userLanguage;
+	if (userLang == "zh-CN" || userLang == "zh-SG" || userLang == "zh-MY" || userLang == "zh-CHS") {
+		userLang = "zh-Hans";
+	} else if (userLang == "zh-HK" || userLang == "zh-MO" || userLang == "zh-TW" || userLang == "zh-CHT") {
+		userLang = "zh-Hant";
+	} else if (userLang.length > 2) {
+		userLang = userLang[0] + userLang[1];
+	}
+	return userLang;
+}
+
+function replace_text(lang, text, objTrls) {
+	var y = document.querySelectorAll("#"+text);
+
+	if (y.length > 0) {
+		for (var j=0; j < y.length; j++) {
+
+			if(y[j].placeholder != undefined) {
+				y[j].placeholder = objTrls[lang][text];
+			} else {
+				y[j].innerHTML = objTrls[lang][text];
+			}
+		}
+	}  else {
+		console.log("element not Found: " + text);
+	}
+}
+
+function translation_available(objTrls, lang) {
+	if (objTrls[lang]) {
+		return lang;
+	} else {
+		console.log("Translation not Found: " + lang + ". Used default");
+		return "source";
+	}
+}
+
+function translate(siteLang, objTrls) {
+	var lang = siteLang ? siteLang : browserLang;
+	var availableLang = translation_available(objTrls, lang);
+
+	var body = document.getElementsByTagName('body')[0];
+	body.classList.add(lang);
+	body.classList.add('style' + extTpl);
+
+	if(rtlLangs.indexOf(lang) !== -1) {
+		body.classList.add('rtl');
+		body.style.direction = 'rtl';
+	}
+
+	if (availableLang) {
+		for (var x in objTrls["source"]) {
+			replace_text(availableLang, x, objTrls);
+		}
+	}
+}
+
+function getParameterByName(name, defaultValue) {
+	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search),
+		value = defaultValue ? defaultValue : "";
+	return results === null ? value : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+docReady(function() {
+	translate(siteLang, translation);
+});
